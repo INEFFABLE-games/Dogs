d605c9c4cf186b6ba1f17a707dd3dd7d56baf671
Anthony <ZetoX3429@mail.ru>
2021-08-30 15:15:50 +0300

    fix: deleted dog constructor

--- internal/models/types.go
+++ internal/models/types.go
@@ -5,11 +5,3 @@ type Dog struct {
 	Name   string `json:"name" validate:"required"`
 	Gender string `json:"gender" validate:"required"`
 }
-
-// NewDog creates new dog object.
-func NewDog(name string, gender string) Dog {
-	return Dog{
-		Name:   name,
-		Gender: gender,
-	}
-}
