54804d5b01de11604029e1a97408ea5d9fe23246
Anthony <ZetoX3429@mail.ru>
2021-08-31 13:20:21 +0300

    feat: added User struct, Token struct, CustomClaims struct

--- internal/models/types.go
+++ internal/models/types.go
@@ -1,7 +1,28 @@
 package models
 
-// Dog struct for dogs.
+import "github.com/golang-jwt/jwt"
+
+// Dog structure for dog object.
 type Dog struct {
+	Owner  string `json:"owner"`
 	Name   string `json:"name" validate:"required"`
 	Gender string `json:"gender" validate:"required"`
+}
+
+// User structure for user object.
+type User struct {
+	Login    string `json:"login" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+// Token structure for token object.
+type Token struct {
+	Login string `json:"login" validate:"required"`
+	Value string `json:"value" validate:"required"`
+}
+
+// CustomClaims structure for jwt token generation claim.
+type CustomClaims struct {
+	Login string
+	jwt.StandardClaims
 }
