721e0fe6e5f7ce173e21c571cb8fc538c12037e4
Anthony <ZetoX3429@mail.ru>
2021-09-01 21:28:17 +0300

    fix: changed functions params. refactor: renamed som functions.

--- internal/service/dogService.go
+++ internal/service/dogService.go
@@ -13,56 +13,56 @@ type DogService struct {
 }
 
 // Create calls add func on dog repository.
-func (p *DogService) Create(ctx context.Context, dog models.Dog, token string) error {
+func (p *DogService) Create(ctx context.Context, dog models.Dog, token string, userLogin string) error {
 
-	result, err := p.dogRepo.ValidateToken(ctx, token)
+	_, err := p.CheckToken(ctx, token)
 	if err != nil {
 		return errors.New("unable to validate token")
 	}
 
-	return p.dogRepo.Create(ctx, dog, result.Login)
+	return p.dogRepo.Create(ctx, dog, userLogin)
 }
 
-// ValidateToken check is token exists.
-func (p *DogService) ValidateToken(ctx context.Context, token string) (models.Token, error) {
-	return p.dogRepo.ValidateToken(ctx, token)
+// CheckToken check is token exists.
+func (p *DogService) CheckToken(ctx context.Context, token string) (models.Token, error) {
+	return p.dogRepo.CheckToken(ctx, token)
 }
 
 // Get call get func on dog repository.
-func (p *DogService) Get(ctx context.Context, token string) (models.Dog, error) {
+func (p *DogService) Get(ctx context.Context, token string, userLogin string) (models.Dog, error) {
 
-	result, err := p.dogRepo.ValidateToken(ctx, token)
+	_, err := p.CheckToken(ctx, token)
 	if err != nil {
 		return models.Dog{}, errors.New("unable to validate token")
 	}
 
-	return p.dogRepo.Get(ctx, result.Login)
+	return p.dogRepo.Get(ctx, userLogin)
 }
 
 // Change call change func on dog repository.
 func (p *DogService) Change(ctx context.Context, token string, dog models.Dog) error {
-	result, err := p.dogRepo.ValidateToken(ctx, token)
+	resultToken, err := p.CheckToken(ctx, token)
 	if err != nil {
 		return errors.New("unable to validate token")
 	}
 
-	return p.dogRepo.Change(ctx, result.Login, dog)
+	return p.dogRepo.Change(ctx, resultToken.Login, dog)
 }
 
 // Delete call delete func on dog repository.
-func (p *DogService) Delete(ctx context.Context, token string) error {
-	result, err := p.dogRepo.ValidateToken(ctx, token)
+func (p *DogService) Delete(ctx context.Context, token string, userLogin string) error {
+	_, err := p.CheckToken(ctx, token)
 	if err != nil {
 		return errors.New("unable to validate token")
 	}
 
 	// check is dog exist
-	_, err = p.dogRepo.Get(ctx, result.Login)
+	_, err = p.dogRepo.Get(ctx, userLogin)
 	if err != nil {
 		return errors.New("dog doesn't exist")
 	}
 
-	return p.dogRepo.Delete(ctx, result.Login)
+	return p.dogRepo.Delete(ctx, userLogin)
 }
 
 // NewDogService creates new dog service.
