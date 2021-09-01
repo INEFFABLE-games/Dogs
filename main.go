66f1ea4dd19ec77f3ce1e761e59cc37a66d74fd6
Anthony <ZetoX3429@mail.ru>
2021-09-01 21:24:03 +0300

    feat: added JWTConfig, middleware functions to check token and get user data from token.

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
+	middleware2 "github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
 	"main/internal/config"
 	"main/internal/handler"
+	"main/internal/middleware"
+	"main/internal/models"
 	"main/internal/repository"
 	"main/internal/service"
 	"net/http"
@@ -17,13 +20,6 @@ import (
 type server struct {
 	dogRepo  repository.DogRepository
 	userRepo repository.UserRepository
-}
-
-func newServer(dogRepo *repository.DogRepository, userRepo *repository.UserRepository) *server {
-	return &server{
-		dogRepo:  *dogRepo,
-		userRepo: *userRepo,
-	}
 }
 
 type customValidator struct {
@@ -43,7 +39,7 @@ func main() {
 	log.SetLevel(log.DebugLevel)
 	cfg := config.NewConfig()
 
-	base, err := sql.Open("postgres", fmt.Sprintf("port=%s host=%s user=%s password=%s dbname=%s sslmode=%s",
+	conn, err := sql.Open("postgres", fmt.Sprintf("port=%s host=%s user=%s password=%s dbname=%s sslmode=%s",
 		cfg.Port,
 		cfg.Host,
 		cfg.User,
@@ -54,15 +50,15 @@ func main() {
 		log.Errorf("main: unable to open sql connection %v,", err)
 	}
 
-	if err := base.Ping(); err != nil {
+	if err := conn.Ping(); err != nil {
 		log.Errorf("main: unable to ping sql connection %v,", err)
 	}
 
-	s := newServer(repository.NewDogRepository(base), repository.NewUserRepository(base))
-	dogService := service.NewDogService(s.dogRepo)
+	//s := newServer(repository.NewDogRepository(base), repository.NewUserRepository(base))
+	dogService := service.NewDogService(*repository.NewDogRepository(conn))
 	dogHandler := handler.NewDogHandler(dogService)
 
-	userService := service.NewUserService(s.userRepo)
+	userService := service.NewUserService(*repository.NewUserRepository(conn))
 	userHandler := handler.NewUserHandler(userService)
 
 	e := echo.New()
@@ -71,16 +67,22 @@ func main() {
 	dogsPath := "/dogs/"
 	usersPath := "/users/"
 
+	jwtConfig := middleware2.JWTConfig{
+		SigningKey: []byte("dog"),
+		Claims:     &models.CustomClaims{},
+		ContextKey: "Data",
+	}
+
 	//dog routs
-	e.PUT(dogsPath, dogHandler.Create)
-	e.GET(dogsPath, dogHandler.Get)
-	e.POST(dogsPath, dogHandler.Change)
-	e.DELETE(dogsPath, dogHandler.Delete)
+	e.PUT(dogsPath, dogHandler.Create, middleware2.JWTWithConfig(jwtConfig), middleware.SetUserData(jwtConfig))
+	e.GET(dogsPath, dogHandler.Get, middleware2.JWTWithConfig(jwtConfig), middleware.SetUserData(jwtConfig))
+	e.POST(dogsPath, dogHandler.Change, middleware2.JWTWithConfig(jwtConfig), middleware.SetUserData(jwtConfig))
+	e.DELETE(dogsPath, dogHandler.Delete, middleware2.JWTWithConfig(jwtConfig), middleware.SetUserData(jwtConfig))
 
 	// user routs
 	e.PUT("/users/registration/", userHandler.Create)
 	e.PUT(usersPath, userHandler.Login)
-	e.DELETE(usersPath, userHandler.Logout)
+	e.DELETE(usersPath, userHandler.Logout, middleware2.JWTWithConfig(jwtConfig), middleware.SetUserData(jwtConfig))
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
