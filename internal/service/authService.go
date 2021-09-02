f063b82edb942e3bedf835607f4ea6950e59a97a
Anthony <ZetoX3429@mail.ru>
2021-09-02 18:50:00 +0300

    feat: added RefreshTokens implementation

--- internal/service/authService.go
+++ internal/service/authService.go
@@ -1,14 +1,36 @@
 package service
 
 import (
+	"context"
 	"github.com/dgrijalva/jwt-go"
 	"main/internal/models"
+	"main/internal/repository"
 	"time"
 )
 
-func CreateToken(userLogin string) (string, error) {
+type AuthService struct {
+	tokenRepo repository.TokenRepository
+}
 
+// RefreshTokens refresh existing tokens for user
+func (a *AuthService) RefreshTokens(ctx context.Context, userLogin string) (string, string, error) {
+
+	//JWT claims
 	claims := models.CustomClaims{
+		Login: userLogin,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute * 1).Unix(),
+			Issuer:    "",
+		},
+	}
+
+	jwtTok, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte("dog"))
+	if err != nil {
+		return "", "", err
+	}
+
+	//RT claims
+	claims = models.CustomClaims{
 		Login: userLogin,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
@@ -16,10 +38,14 @@ func CreateToken(userLogin string) (string, error) {
 		},
 	}
 
-	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte("dog"))
-	if err != nil {
-		return "", err
-	}
+	rt, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte("RefTokKey"))
 
-	return token, err
+	err = a.tokenRepo.InsertOrUpdate(ctx, rt, userLogin)
+
+	return jwtTok, rt, err
+}
+
+// NewAuthService creates new AuthService object
+func NewAuthService(tokenRepo repository.TokenRepository) *AuthService {
+	return &AuthService{tokenRepo: tokenRepo}
 }
