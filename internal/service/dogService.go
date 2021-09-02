2b5417c1a8d9a205b881ef59b27ae044169cab1f
Anthony <ZetoX3429@mail.ru>
2021-09-02 15:08:50 +0300

    fix: deleted checkToken func

--- internal/service/dogService.go
+++ internal/service/dogService.go
@@ -13,14 +13,8 @@ type DogService struct {
 }
 
 // Create calls add func on dog repository.
-func (p *DogService) Create(ctx context.Context, dog models.Dog, token string, userLogin string) error {
-
-	_, err := p.CheckToken(ctx, token)
-	if err != nil {
-		return errors.New("unable to validate token")
-	}
-
-	return p.dogRepo.Create(ctx, dog, userLogin)
+func (p *DogService) Create(ctx context.Context, dog models.Dog) error {
+	return p.dogRepo.Create(ctx, dog)
 }
 
 // CheckToken check is token exists.
@@ -29,35 +23,19 @@ func (p *DogService) CheckToken(ctx context.Context, token string) (models.Token
 }
 
 // Get call get func on dog repository.
-func (p *DogService) Get(ctx context.Context, token string, userLogin string) (models.Dog, error) {
-
-	_, err := p.CheckToken(ctx, token)
-	if err != nil {
-		return models.Dog{}, errors.New("unable to validate token")
-	}
-
+func (p *DogService) Get(ctx context.Context, userLogin string) (models.Dog, error) {
 	return p.dogRepo.Get(ctx, userLogin)
 }
 
 // Change call change func on dog repository.
-func (p *DogService) Change(ctx context.Context, token string, dog models.Dog) error {
-	resultToken, err := p.CheckToken(ctx, token)
-	if err != nil {
-		return errors.New("unable to validate token")
-	}
-
-	return p.dogRepo.Change(ctx, resultToken.Login, dog)
+func (p *DogService) Change(ctx context.Context, dog models.Dog) error {
+	return p.dogRepo.Change(ctx, dog)
 }
 
 // Delete call delete func on dog repository.
-func (p *DogService) Delete(ctx context.Context, token string, userLogin string) error {
-	_, err := p.CheckToken(ctx, token)
-	if err != nil {
-		return errors.New("unable to validate token")
-	}
-
+func (p *DogService) Delete(ctx context.Context, userLogin string) error {
 	// check is dog exist
-	_, err = p.dogRepo.Get(ctx, userLogin)
+	_, err := p.dogRepo.Get(ctx, userLogin)
 	if err != nil {
 		return errors.New("dog doesn't exist")
 	}
